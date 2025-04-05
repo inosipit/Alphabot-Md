40e614025d4993e75dae24fc5e56bc0c817a0488
inodev <inosipit83@gmail.com>
2025-04-06 00:45:32 +0700

    Update settings.js
    p

--- settings.js
+++ settings.js
@@ -25,7 +25,7 @@ edit sewabot di file settings.js line 24
 `
 // setting 
 global.firtsChat = true //send firts message to new user
-global.autoread = true // auto read message
+global.autoread = false // auto read message
 global.antidelete = false // anti delete
 global.antiviewonce = true // anti view once
 global.autobio = false //auto ganti bio
